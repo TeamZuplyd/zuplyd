39ebba49aab13bca1c527009a37c09e2fbf5f8e7
Isuru Nishadha <73130204+IsuruNish@users.noreply.github.com>
2022-08-01 23:36:49 +0530

    Create The Contact Details Form Component For Company Admin

--- apps/frontend/src/app/pages/testPage1.tsx
+++ apps/frontend/src/app/pages/testPage1.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import FormComponent from '../components/form-component/form-component';
 
 function testPage1() {
   return (
     <div>
-      <FormComponent/>
       Welcome to testPage One
     </div>
   );
