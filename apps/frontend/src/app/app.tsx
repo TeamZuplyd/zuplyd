2319b537e157a9432c9002333936f5e8e751e76d
ThisariG <thisarigun99@gmail.com>
2022-08-04 14:57:40 +0530

    Testing components

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -14,21 +14,21 @@ import Header from './components/header/header';
 
 export function App() {
   return (
+    // <ForgetPassword/>
 
-  <ForgetPassword/>
-
-    // <div className="outerContainer">
-    //   <SideNav username={'Amarabandu Rupasingheeeee'} userNum={0} />
-    //   {/* <div className="header">Header goes here</div> */}
-    //   <Header title={"Dashboard" } />
-    //   <div className="content">
-    //     <Routes>
-    //       {/* <Route path="/" element={} /> */}
-    //       <Route path="/page1" element={<TestPage1 />} />
-    //       <Route path="/page2" element={<TestPage2 />} />
-    //     </Routes>
-    //   </div>
-    // </div>
+    <div className="outerContainer">
+      <SideNav username={'Amarabandu Rupasingheeeee'} userNum={0} />
+      {/* <div className="header">Header goes here</div> */}
+      <Header title={'Dashboard'} />
+      <div className="content">
+        <Routes>
+          {/* <Route path="/" element={} /> */}
+          <Route path="/page1" element={<TestPage1 />} />
+          <Route path="/page2" element={<TestPage2 />} />
+          {/* <Route path="/company" element={<TestPage3 />} /> */}
+        </Routes>
+      </div>
+    </div>
   );
 }
 
