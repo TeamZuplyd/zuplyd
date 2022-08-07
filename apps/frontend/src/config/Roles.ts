2801af25f834c622dd5d79c753f651078ffb4793
ThisariG <thisarigun99@gmail.com>
2022-08-07 22:46:39 +0530

    Add Roles Config File

---
+++ apps/frontend/src/config/Roles.ts
@@ -0,0 +1,10 @@
+//To use enums, import them into the component and use them as follows:
+//import { ROLES } from './config/Roles';
+//console.log(ROLES.comp_admin);
+
+export enum ROLE {
+  comp_admin = 'Company Admin',
+  wh_mngr = 'Warehouse Manager',
+  sh_mngr = 'Shop Manager',
+  supl = 'Supplier',
+}
