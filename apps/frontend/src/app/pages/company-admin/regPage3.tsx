7ef64e5b8878cf19a1a93f1fb0d4727612956723
ThisariG <thisarigun99@gmail.com>
2022-08-12 01:50:24 +0530

    Add Click Event on the Distribution Structure Card

--- apps/frontend/src/app/pages/company-admin/regPage3.tsx
+++ apps/frontend/src/app/pages/company-admin/regPage3.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Grid from '@mui/material/Grid';
 import { Typography } from '@mui/material';
 import Button from '@mui/material/Button';
@@ -8,99 +8,116 @@ import CardMedia from '@mui/material/CardMedia';
 import { CardActionArea } from '@mui/material';
 import { BrowserRouter, Route, Routes, Link, useNavigate } from 'react-router-dom';
 // import { useQuery } from 'react-query';
-
-
+import { useState } from 'react';
 
 function regPage3() {
   const navigate = useNavigate();
- 
- 
+  const [selected, setSelected] = useState(0);
+  const distributionStructure = [
+    {
+      id: 1,
+      name: 'Centralized',
+      description: 'A single top level warehouse collects goods from suppliers and distributes to other warehouses',
+      img: '../../assets/imgs/c.png',
+    },
+    {
+      id: 2,
+      name: 'Distributed',
+      description: 'A single top level warehouse collects goods from suppliers and distributes to other warehouses',
+      img: '../../assets/imgs/c.png',
+    },
+    ];
+    const handleSelected = (index: number) => {
+      setSelected(index);
+      console.log('selected ' + index);
+    };
   return (
     <Grid container rowGap={0} columnGap={0}>
-
-    <Grid container item xs={6} alignItems="center"  justifyContent="center" style={{minHeight:"100vh"}}>  
-    
-        <Grid container item xs={6} direction="column"  rowSpacing={2} style={{ maxWidth: '50%' }}>  
-            <Grid  item xs={6}>  
-                <Button onClick={() => navigate('/comp-init-1', {replace:true})} variant="contained" className='goBack' style={{width: "30%"}}>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} style={{width:"25"}}>
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M11 17l-5-5m0 0l5-5m-5 5h12" />
-                </svg>
-                    Go Back
-                </Button>
-            </Grid>
-
-            <Grid  item xs={6}>  
-                <Typography variant="h4" sx={{ fontWeight: 500, fontSize: 36}} >
-                    Select the distribution structure.
-                </Typography>
-            </Grid>
-
-
-            <Grid  item xs={6}>  
-                <Card sx={{ maxWidth: "100%", padding:"2%", backgroundColor:"#e6e1e1", mt:5}} >
-                    <Grid container xs={12}>
-                        <Grid item direction="column" xs={4}>
-                            <img src="../../assets/imgs/c.png" alt=""  style={{width:100}}/>
-                        </Grid>
-
-                        <Grid item direction="column" xs={8}>
-                            <Typography gutterBottom variant="h5" component="div" sx={{fontSize: 20}}>
-                                Centralized
-                            </Typography>
-
-                            <Typography variant="body2" color="text.secondary">
-                                A single top level warehouse collects goods from suppliers and distributes to other warehouses
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                </Card>
-            </Grid>
-
-            <Grid  item xs={6}>  
-                <Card sx={{ maxWidth: "100%", padding:"2%",  backgroundColor:"#ffe4c4", mt:3 }}>
-                        <Grid container xs={12}>
-                            <Grid item direction="column" xs={4}>
-                                <img src="../../assets/imgs/c.png" alt=""  style={{width:100}}/>
-                            </Grid>
-
-                            <Grid item direction="column" xs={8}>
-                                <Typography gutterBottom variant="h5" component="div" sx={{fontSize: 20}}>
-                                    Centralized
-                                </Typography>
-
-                                <Typography variant="body2" color="text.secondary">
-                                    A single top level warehouse collects goods from suppliers and distributes to other warehouses
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                    </Card>
-            </Grid>
-
-        
-            {/* <Link to="/comp-init-3"> */}
-            <Button variant="contained" className='createAcc' style={{width: "100%",marginTop:"20%"}}>
-                Continue
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} style={{width:"25", marginLeft:"20"}}>
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
-                </svg>
+      <Grid container item xs={6} alignItems="center" justifyContent="center" style={{ minHeight: '100vh' }}>
+        <Grid container item xs={6} direction="column" rowSpacing={2} style={{ maxWidth: '50%' }}>
+          <Grid item xs={6}>
+            <Button onClick={() => navigate('/comp-init-1', { replace: true })} variant="contained" className="goBack" style={{ width: '30%' }}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} style={{ width: '25' }}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M11 17l-5-5m0 0l5-5m-5 5h12" />
+              </svg>
+              Go Back
             </Button>
-            {/* </Link> */}
+          </Grid>
+
+          <Grid item xs={6}>
+            <Typography variant="h4" sx={{ fontWeight: 500, fontSize: 36 }}>
+              Select the distribution structure.
+            </Typography>
+          </Grid>
+
+          <Grid item container spacing={2} justifyContent="space-between" alignItems="center" direction="column">
+            {distributionStructure.map((item, index) => {
+              return (
+                <Grid item xs={6}>
+                      <DistributionStructure
+                          id={distributionStructure[index].id}
+                          name={distributionStructure[index].name}
+                          description={distributionStructure[index].description}
+                          img={distributionStructure[index].img}
+                          handleSelect={handleSelected}
+                          selected={selected} />
+                </Grid>
+              );
+            })}
+          </Grid>
+          {/* <Link to="/comp-init-3"> */}
+          <Button variant="contained" className="createAcc" style={{ width: '100%', marginTop: '20%' }}>
+            Continue
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} style={{ width: '25', marginLeft: '20' }}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+            </svg>
+          </Button>
+          {/* </Link> */}
         </Grid>
-    </Grid>
+      </Grid>
 
-    
-
-    <Grid container item xs={6} alignItems="center"  justifyContent="center" style={{backgroundColor:"#DBDBDB"}}>  
-
-        <Grid item xs={6} >  
-                <img src="../../assets/svg/ca-p2.svg" alt="" style={{width:500}} />
+      <Grid container item xs={6} alignItems="center" justifyContent="center" style={{ backgroundColor: '#DBDBDB' }}>
+        <Grid item xs={6}>
+          <img src="../../assets/svg/ca-p2.svg" alt="" style={{ width: 500 }} />
         </Grid>
-
+      </Grid>
     </Grid>
-
-</Grid>
-  )
+  );
 }
 
-export default regPage3
+export default regPage3;
+
+type distributionStructureProps = {
+  id: number;
+  name: string;
+  description: string;
+  img: string;
+  handleSelect: (id: number) => void;
+  selected: number;
+};
+
+function DistributionStructure({ id, name, description, img, handleSelect, selected }: distributionStructureProps) {
+  return (
+    <Card sx={{ maxWidth: '100%' }} onClick={() => handleSelect(id)} className={selected === id ? 'highlightTier' : ''}>
+      <CardActionArea>
+        <CardContent sx={{ p: 3 }}>
+          <Grid container xs={12}>
+            <Grid item direction="column" xs={4}>
+              <img src={img} alt="" style={{ width: 100 }} />
+            </Grid>
+
+            <Grid item direction="column" xs={8}>
+              <Typography gutterBottom variant="h5" component="div" sx={{ fontSize: 20 }}>
+                {name}
+              </Typography>
+
+              <Typography variant="body2" color="text.secondary">
+                {description}
+              </Typography>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
