458a61c8e3f076c7940bce2fa88ff40bd2a177a2
Isuru Nishadha <73130204+IsuruNish@users.noreply.github.com>
2022-08-12 04:03:31 +0530

    Add Company Adin Company Page

--- apps/frontend/src/app/components/contact-details-form-component/contact-details-form-component.tsx
+++ apps/frontend/src/app/components/contact-details-form-component/contact-details-form-component.tsx
@@ -6,91 +6,205 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { Grid } from '@mui/material';
 import { useState } from 'react';
-export interface ContactDetailsFormComponentProps {}
+import { ArrowNarrowDownIcon } from '@heroicons/react/outline';
 
-export function ContactDetailsFormComponent(props: ContactDetailsFormComponentProps) {
+export interface ContactDetailsFormComponentProps {
+  email: string;
+  phoneNumbers: string[];
+}
+
+export function ContactDetailsFormComponent({
+  email,
+  phoneNumbers,
+}: ContactDetailsFormComponentProps) {
   const [inputState, setInputState] = useState(true);
   const [newPhoneNumber, setNewPhoneNumber] = useState([]);
 
+  const [allNumbers, setAllNumbers] = useState(phoneNumbers);
+  const [theEmail, setTheEmail] = useState(email);
+
   const changeInputState = () => {
-    setInputState(!inputState)
-  }
+    setInputState(!inputState);
+  };
 
   const addNewTextField = () => {
-    const arr: any = [...newPhoneNumber, []]
-    setNewPhoneNumber(arr)
-  }
+    const arr: any = [...newPhoneNumber, []];
+    setNewPhoneNumber(arr);
+  };
 
+  const addPhoneNumber = (index: number) => (e: any) => {
+    const arr: any = [...allNumbers];
+    arr[index] = e.target.value;
+    setAllNumbers(arr);
+  };
 
-  const phoneNumbers: string[] = ["011 2854711", "011 2547899", "077 44185597", "011 2587411"]
-  let count: number = 1
+  const addNewPhoneNumber = (index: number) => (e: any) => {
+    const arr: any = [...newPhoneNumber];
+    arr[index] = e.target.value;
+    setNewPhoneNumber(arr);
+  };
+
+  const discardChanges = () => {
+    setAllNumbers(phoneNumbers);
+    setNewPhoneNumber([]);
+    setTheEmail(email);
+  };
+
+  let count: number = 1;
 
   return (
     <>
-    <Card sx={{ width: "899px", maxWidth:"899px", height: "fit-content",maxHeight: "380px" }}>
-              <CardContent>
+      <Card
+        sx={{
+          width: '899px',
+          maxWidth: '899px',
+          height: 'fit-content',
+          maxHeight: '380px',
+          mt: 3,
+        }}
+      >
+        <CardContent>
+          <Grid container rowGap={2} columnGap={2.5}>
+            <Grid item xs={10.3}>
+              <Typography
+                variant="h5"
+                component="div"
+                sx={{ fontWeight: 700 }}
+                style={{ color: '#1f2937' }}
+              >
+                Conatct Details
+              </Typography>
+            </Grid>
 
-                  <Grid container rowGap={2} columnGap={2.5}>
-                      <Grid item xs={10.3}>
-                            <Typography variant="h5" component="div" sx={{fontWeight: 700}} style={{color:"#1f2937"}}>
-                              Conatct Details
-                            </Typography>
-                      </Grid>
+            <Grid item xs={1}>
+              <Button
+                variant="contained"
+                sx={{ width: 100, backgroundColor: '#9ca3af' }}
+                className="editBtn"
+                onClick={changeInputState}
+              >
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    style={{
+                      height: '15px',
+                      width: '20px',
+                      marginRight: '8px',
+                    }}
+                  >
+                    <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                  </svg>
+                  Edit
+                </div>
+              </Button>
+            </Grid>
+          </Grid>
 
-                      <Grid item xs={1}>
-                          <Button variant="contained" sx={{width:100, backgroundColor:"#9ca3af"}} className="editBtn" onClick={changeInputState}>
-                            <div>
-                                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" style={{height:'15px', width: '20px', marginRight:"8px"}}>
-                                    <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                                  </svg>
-                                  Edit
-                            </div>
-                          </Button>
-                      </Grid>
-                  </Grid>
+          <Grid container rowGap={0} columnGap={1}>
+            {allNumbers.map((no, index) => {
+              return (
+                <Grid item xs={2.5}>
+                  <TextField
+                    id="outlined-basic"
+                    value={no}
+                    onChange={addPhoneNumber(index)}
+                    disabled={inputState}
+                    label={'Conatct #' + count++}
+                    variant="outlined"
+                    size="small"
+                    sx={{ mt: 4, width: '160px' }}
+                  />
+                </Grid>
+              );
+            })}
 
-                  <Grid container rowGap={0} columnGap={1}>
-                      {phoneNumbers.map((no)=> {
-                          return (
-                          <Grid item xs={2.5}> 
-                            <TextField id="outlined-basic" onChange={addNewTextField} defaultValue={no} disabled = {inputState} label={"Conatct #"+count++} variant="outlined" size="small" sx={{mt:4, width: '160px'}} /> 
-                          </Grid>)
-                      })}
+            {newPhoneNumber.map((no, index) => {
+              return (
+                <Grid item xs={2.5}>
+                  <TextField
+                    id="outlined-basic"
+                    value={no}
+                    onChange={addNewPhoneNumber(index)}
+                    disabled={inputState}
+                    label={'Conatct #' + count++}
+                    variant="outlined"
+                    size="small"
+                    sx={{ mt: 4, mb: 5, width: '160px' }}
+                  />
+                </Grid>
+              );
+            })}
 
-                      {newPhoneNumber.map((no)=> {
-                          return (
-                          <Grid item xs={2.5}> 
-                            <TextField id="outlined-basic" defaultValue={no} disabled = {inputState} label={"Conatct #"+count++} variant="outlined" size="small" sx={{mt:4, mb:5, width: '160px'}} /> 
-                          </Grid>)
-                      })}
+            <Grid item xs={1}>
+              <Button
+                variant="contained"
+                sx={{
+                  width: 120,
+                  mt: 4,
+                  mb: 5,
+                  backgroundColor: 'transparent',
+                  color: 'blue',
+                }}
+                className="addnewBtn"
+                onClick={addNewTextField}
+              >
+                {' '}
+                {count - 1 < 5 ? '+ Add New' : ' '}{' '}
+              </Button>
+            </Grid>
+          </Grid>
 
-                        <Grid item xs={1}> 
-                          <Button variant="contained" sx={{width:120, mt:4, mb:5, backgroundColor:"transparent",color:"blue"}} className="addnewBtn"  onClick={addNewTextField} > {count-1<5?"+ Add New": " "} </Button>
-                        </Grid>
-                  </Grid>
+          <Grid container rowGap={0} columnGap={1}>
+            <Grid item xs={2.5}>
+              <TextField
+                id="outlined-basic"
+                onChange={(e) => {
+                  setTheEmail(e.target.value);
+                }}
+                value={theEmail}
+                disabled={inputState}
+                label="Email"
+                variant="outlined"
+                size="small"
+                sx={{ mb: 5, width: '350px' }}
+              />
+            </Grid>
+          </Grid>
 
-                  <Grid container rowGap={0} columnGap={1}>
-                      <Grid item xs={2.5}> 
-                        <TextField id="outlined-basic" defaultValue={"abc@gmail.com"} disabled = {inputState} label="Email" variant="outlined" size="small" sx={{mb:5, width: '350px'}} /> 
-                      </Grid>
-                  </Grid>
+          <Grid container rowGap={2} columnGap={4}>
+            <Grid item xs={2}>
+              <Button
+                variant="contained"
+                color="success"
+                sx={{ width: 140, mb: 2 }}
+                className="saveChangesBtn"
+              >
+                {' '}
+                Save Changes{' '}
+              </Button>
+            </Grid>
 
-
-                  <Grid container rowGap={2} columnGap={4}>
-                      <Grid item xs={2}>
-                          <Button variant="contained" color="success" sx={{width:140, mb:2}} className="saveChangesBtn" > Save Changes </Button>
-                      </Grid>
-
-                      <Grid item xs={5}>
-                          <Button variant="contained" sx={{width:100, backgroundColor:"transparent",color:"black"}} className="addnewBtn" > Discard </Button>
-                      </Grid>
-                  </Grid>
-              </CardContent>
-        </Card>
-    
-    
-    
-    
+            <Grid item xs={5}>
+              <Button
+                variant="contained"
+                sx={{
+                  width: 100,
+                  backgroundColor: 'transparent',
+                  color: 'black',
+                }}
+                className="addnewBtn"
+                onClick={discardChanges}
+              >
+                {' '}
+                Discard{' '}
+              </Button>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
     </>
   );
 }
--- apps/frontend/src/app/pages/company-admin/company.tsx
+++ apps/frontend/src/app/pages/company-admin/company.tsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React from 'react';
+import Header from '../../components/header/header';
+import { Grid } from '@mui/material';
+import FormComponent from '../../components/form-component/form-component';
+import ContactDetailsFormComponent from '../../components/contact-details-form-component/contact-details-form-component';
 
-function company() {
-  return (
-    <div>company</div>
-  )
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+import RegPage4 from '../../pages/company-admin/regPage4';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
 }
 
-export default company
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+function company() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <>
+      <Header title={'Dashboard'} />
+
+      <div className="content">
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+          >
+            <Tab label="Details" {...a11yProps(0)} />
+            <Tab label="Plan & Billing" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          <FormComponent companyName="Zuplyd" companyAddress="" />
+
+          <ContactDetailsFormComponent
+            email="zuplyD@gmail.com"
+            phoneNumbers={['0766811254', '0711478525']}
+          />
+        </TabPanel>
+
+        <TabPanel value={value} index={1}>
+          {/* <RegPage4 /> */}
+        </TabPanel>
+      </div>
+    </>
+  );
+}
+
+export default company;
