3c3dcfc5e328897ed1c0ce1c21b317116cfd9dd8
ThisariG <thisarigun99@gmail.com>
2022-08-12 21:57:32 +0530

    Updated Routes

---
+++ apps/frontend/src/app/components/category-card/category-card.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable-next-line */
+export interface CategoryCardProps {
+  name: string;
+}
+
+export function CategoryCard({ name }: CategoryCardProps) {
+  return (
+    <div>
+      <h1>Welcome to CategoryCard!</h1>
+    </div>
+  );
+}
+
+export default CategoryCard;
