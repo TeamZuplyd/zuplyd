5a705437cd2e5c92787412d7a543b5d030c5a80c
ThisariG <thisarigun99@gmail.com>
2022-08-20 11:43:38 +0530

    Changing The Structure of Good Request Card

--- apps/frontend/src/app/components/item-card/item-card.tsx
+++ apps/frontend/src/app/components/item-card/item-card.tsx
@@ -1,24 +1,21 @@
 /* eslint-disable-next-line */
-import { Card, CardContent, Grid, Typography } from '@mui/material';
+import { Card, CardContent, Grid, Typography, Button } from '@mui/material';
 
 export interface ItemCardProps {
   urgencyLevel?: string;
   item: string;
   warehouse: string;
   quantity: number;
+  requiredBy: string;
+  assigned?: boolean;
 }
 
-export function ItemCard({
-  urgencyLevel = 'High',
-  item,
-  warehouse,
-  quantity,
-}: ItemCardProps) {
+export function ItemCard({ urgencyLevel = 'High', item, warehouse, quantity, requiredBy, assigned = false }: ItemCardProps) {
   return (
     <Card
       sx={{
         p: 0,
-        m: 1,
+        m: 0,
         minWidth: 370,
         width: 370,
         maxHeight: 200,
@@ -30,50 +27,50 @@ export function ItemCard({
           p: 0,
           minWidth: 370,
           width: 370,
-          maxHeight: 200,
-          height: 200,
+          maxHeight: 250,
+          height: 250,
           '&:last-child': {
             paddingBottom: 0,
           },
         }}
       >
-        <Grid
-          container
-          wrap="nowrap"
-          sx={{ p: 0 }}
-          justifyContent="space-around"
-          alignItems="center"
-        >
+        <Grid container wrap="nowrap" sx={{ p: 0 }} justifyContent="space-around" alignItems="center">
           <Grid item xs={11}>
             <Grid sx={{ flexGrow: 1, pl: 2 }} container spacing={2}>
-              <Grid item xs={8}>
+              <Grid item xs={7}>
                 <Typography className="secondaryText">Item</Typography>
-                <Typography className="primaryText">{item} </Typography>
+                <Typography sx={{ wordWrap: 'break-word' }} className="primaryText">
+                  {item}
+                </Typography>
               </Grid>
-              <Grid item xs={4}>
+              <Grid item xs={5}>
                 <Typography className="secondaryText">Quantity</Typography>
-                <Typography className="primartText">
+                <Typography sx={{ wordWrap: 'break-word' }} className="primartText">
                   {quantity + ' pcs'}
                 </Typography>
               </Grid>
-              <Grid item sx={{ alignItems: 'flex-start' }} xs={12}>
+              <Grid item xs={7}>
                 <Typography className="secondaryText">Warehouse</Typography>
-                <Typography className="primaryText">{warehouse}</Typography>
+                <Typography sx={{ wordWrap: 'break-word' }} className="primaryText">
+                  {warehouse}
+                </Typography>
+              </Grid>
+              <Grid item xs={5} alignContent="center">
+                <Typography className="secondaryText">Required By</Typography>
+                <Typography className="primaryText">{requiredBy}</Typography>
+              </Grid>
+              <Grid item xs={7}>
+                {assigned && <Button sx={{ p: 0 }}>Send Quotations</Button>}
+              </Grid>
+              <Grid item xs={5}>
+                {(assigned && <Button sx={{ p: 0 }}>Unassign</Button>) || (!assigned && <Button sx={{ p: 0 }}>Assign to me</Button>)}
               </Grid>
             </Grid>
           </Grid>
           <Grid
             item
             xs={1}
-            bgcolor={
-              urgencyLevel === 'High'
-                ? '#F60303'
-                : urgencyLevel === 'Moderate'
-                ? '#FD8F02'
-                : urgencyLevel === 'Normal'
-                ? '#24CC08'
-                : 'white'
-            }
+            bgcolor={urgencyLevel === 'High' ? '#F60303' : urgencyLevel === 'Moderate' ? '#FD8F02' : urgencyLevel === 'Normal' ? '#24CC08' : 'white'}
             sx={{
               p: 0,
               m: 0,
