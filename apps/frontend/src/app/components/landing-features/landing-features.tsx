f118b6d9fad6ada8d62893ea10f718913aeaeddc
Dumindu <dumindu.chath@gmail.com>
2022-08-21 09:24:04 -0700

    Fix style errors

--- apps/frontend/src/app/components/landing-features/landing-features.tsx
+++ apps/frontend/src/app/components/landing-features/landing-features.tsx
@@ -1,8 +1,9 @@
+import { CSSProperties } from "react";
+
 import Container from '@mui/material/Container';
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
-
 import TaskIcon from '@mui/icons-material/Task';
 import NotificationImportantIcon from '@mui/icons-material/NotificationImportant';
 import TuneIcon from '@mui/icons-material/Tune';
@@ -19,7 +20,7 @@ export function LandingFeatures(props: LandingFeaturesProps) {
     Aos.init({duration:1000});
   },[]);
 
-  const fBox1 ={
+  const fBox1:CSSProperties ={
     background: "linear-gradient(110.5deg, rgba(14, 164, 248, 0.4) 3.13%, rgba(255, 0, 214, 0.16) 101.4%)",
     backdropFilter: "blur(40px)",
     borderRadius: "16px",
@@ -28,7 +29,7 @@ export function LandingFeatures(props: LandingFeaturesProps) {
     padding:"20px"
   }
 
-  const fBox2 ={
+  const fBox2:CSSProperties ={
     background: "linear-gradient(146.17deg, rgba(111, 252, 0, 0.4) 1.28%, rgba(0, 255, 224, 0.16) 102.44%)",
     backdropFilter: "blur(40px)",
     borderRadius: "16px",
@@ -41,7 +42,7 @@ export function LandingFeatures(props: LandingFeaturesProps) {
     left:"-70px"
   }
 
-  const fBox3 ={
+  const fBox3:CSSProperties ={
     background: "linear-gradient(100.91deg, rgba(255, 247, 49, 0.4) 0%, rgba(248, 238, 0, 0.204) 100.48%)",
     backdropFilter: "blur(40px)",
     borderRadius: "16px",
@@ -54,7 +55,7 @@ export function LandingFeatures(props: LandingFeaturesProps) {
     right:"20px"
   }
 
-  const fBox4 ={
+  const fBox4:CSSProperties ={
     background: "linear-gradient(131.49deg, rgba(255, 72, 72, 0.4) 15.58%, rgba(252, 142, 14, 0.4) 78.16%)",
     backdropFilter: "blur(40px)",
     borderRadius: "16px",
@@ -67,7 +68,7 @@ export function LandingFeatures(props: LandingFeaturesProps) {
     left:"-110px"
   }
 
-  const fBox5 ={
+  const fBox5:CSSProperties ={
     background: "linear-gradient(141.75deg, rgba(5, 255, 225, 0.4) 11.75%, rgba(50, 146, 235, 0.16) 97.88%)",
     backdropFilter: "blur(40px)",
     borderRadius: "16px",
@@ -160,8 +161,8 @@ export function LandingFeatures(props: LandingFeaturesProps) {
                 display="flex"
                 justifyContent="center"
                 alignItems="center"
-                sx={{ flexDirection: "column", mt:6 }}
-                style={fBox3}
+                sx={{ flexDirection: "column", mt:6, ...fBox3 }}
+                
                 data-aos="fade-left"
               >
                 <TaskIcon sx={{ color: "#FFF",fontSize: 63, mb:2 }}/>
