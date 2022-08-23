ad00fb926c01dd06bc16b77ef425e51be02f4de8
Kasun M <kasunldrago77@gmail.com>
2022-08-23 12:21:22 +0530

    Add DB integration

--- apps/request-handler/src/app/app.module.ts
+++ apps/request-handler/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ShopWarehouseModule } from './shop-warehouse/shop-warehouse.module';
 
 @Module({
-  imports: [ShopWarehouseModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://admin:fyDl5TP8g8DGc25G@requesthandler.4bbyhlw.mongodb.net/?retryWrites=true&w=majority'), ShopWarehouseModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/request-handler/src/app/shop-warehouse/shop-warehouse.module.ts
+++ apps/request-handler/src/app/shop-warehouse/shop-warehouse.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ShopWarehouseService } from './shop-warehouse.service';
 import { ShopWarehouseController } from './shop-warehouse.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ShopWarehouseReq, ShopWarehouseReqSchema } from './schemas/shop-warehouse-req.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: ShopWarehouseReq.name, schema: ShopWarehouseReqSchema }])],
   providers: [ShopWarehouseService],
   controllers: [ShopWarehouseController],
 })
