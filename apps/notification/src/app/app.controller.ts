65312982d587dfe60fc3a3c10b2c4821171fde59
SCGallage <gallagesanka03@gmail.com>
2022-08-23 23:27:27 +0530

    button color changed

--- apps/notification/src/app/app.controller.ts
+++ apps/notification/src/app/app.controller.ts
@@ -21,7 +21,7 @@ export class AppController {
   @Post('bulk')
   sendEmailBulk(@Body('comp_name') comp_name, @Body('p_managers') p_managers, @Body('w_managers') w_managers, @Body('s_managers') s_managers) {
     this.appService.sendEmailBulk(comp_name, 'Procurement Manager', p_managers);
-    // this.appService.sendEmailBulk(comp_name, 'Warehouse Manager', w_managers);
-    // this.appService.sendEmailBulk(comp_name, 'Shop Manager', s_managers);
+    this.appService.sendEmailBulk(comp_name, 'Warehouse Manager', w_managers);
+    this.appService.sendEmailBulk(comp_name, 'Shop Manager', s_managers);
   }
 }
