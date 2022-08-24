7a2dc7a5ba1f365f8e9eedfebe8748314cb2a126
daredevil25 <devindsnk25@gmail.com>
2022-08-24 11:06:51 +0530

    Procurement measure unit change

--- apps/procurement/src/app/item/dto/create-item-type.dto.ts
+++ apps/procurement/src/app/item/dto/create-item-type.dto.ts
@@ -3,6 +3,7 @@ export class CreateItemTypeDto {
   readonly category_name: string;
   readonly brand_name: string;
   readonly min_release_quantity: number;
+  readonly min_release_quantity_unit: string;
   readonly output_rule: string;
   readonly output_rule_unit: string;
   readonly company_id: string;
