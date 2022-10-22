aebd9b29498b434f4bbb9dc60be775fb1366065a
Isuru Nishadha <73130204+IsuruNish@users.noreply.github.com>
2022-10-22 16:32:51 +0530

    Add Low Stocks UI For Warehouse Manager

--- apps/frontend/src/app/pages/warehouse-manager/orders.tsx
+++ apps/frontend/src/app/pages/warehouse-manager/orders.tsx
@@ -3,6 +3,7 @@ import { TextField, Grid, Button, Tabs, Tab, Typography, Box, Modal, Card, CardC
 import Header from '../../components/header/header';
 import OrderTable from '../../components/order-table/order-table';
 import PMOrderTable from '../../components/pmorder-table/pmorder-table';
+import WMlowStocksTable from '../../components/wmlow-stocks-table/wmlow-stocks-table';
 // import { orders as data } from '../../../data/orders';
 import axios from 'axios';
 import React from 'react';
@@ -39,6 +40,10 @@ function orders() {
   const [orderData, setOrderData] = useState<any>([]);
   const [open, setOpen] = useState(false);
 
+  const [selected, setSelected] = useState(-1); //selected Warehouse
+  const [selectedItem, setSelectedItem] = useState(''); //selected Item
+  const [sentRequest, setSentRequest] = useState<any>([]);
+
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
@@ -62,6 +67,28 @@ function orders() {
     console.log(e.target.value);
     console.log(item);
   };
+
+  const handleStatusChange2 = (warehouseIndex: number, selectedItem: string) => {
+    setSelected(-1);
+    const index = orderData.findIndex((item) => item.item_code === selectedItem); //selected request
+    const newOrderData = orderData[index]; //backend data object
+    // newOrderData['warehouse_id'] = warehouses[warehouseIndex];
+    newOrderData['sentRequest'] = true;
+    setOrderData([...orderData.slice(0, index), newOrderData, ...orderData.slice(index + 1)]);
+    console.log(orderData);
+  };
+
+  const handleSelected = (index: number) => {
+    setSelected(index);
+    console.log('selected ' + index);
+  };
+
+  const addSentRequest = (request: any) => {
+    console.log(request);
+    setSentRequest([...sentRequest, ...request]);
+    //SentRequest -> backend object
+  };
+
   return (
     <>
       <Header title={'Orders'} />
@@ -69,23 +96,30 @@ function orders() {
         <Box sx={{ width: '100%', paddingLeft: '2rem' }}>
           <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
             <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" textColor="primary" indicatorColor="primary">
-              <Tab label="Received Requests" {...a11yProps(0)} />
-              <Tab label="Sent Requests" {...a11yProps(1)} />
-              <Tab label="History" {...a11yProps(2)} />
+              <Tab label="Low Stocks" {...a11yProps(0)} />
+              <Tab label="Received Requests" {...a11yProps(1)} />
+              <Tab label="Sent Requests" {...a11yProps(2)} />
+              <Tab label="History" {...a11yProps(3)} />
             </Tabs>
           </Box>
-          {/* Received Requests */}
+
+          {/* Low Stocks */}
           <TabPanel value={value} index={0}>
+            <WMlowStocksTable orders={orderData} handleStatusChange={() => handleStatusChange2(selected, selectedItem)} selected={selected} setSelected={setSelected} handleSelected={handleSelected} selectedItem={selectedItem} setSelectedItem={setSelectedItem} addSentRequest={addSentRequest} />
+          </TabPanel>
+
+          {/* Received Requests */}
+          <TabPanel value={value} index={1}>
             <OrderTable orders={orderData} handleStatusChange={handleStatusChange} />
           </TabPanel>
 
           {/* Sent Requests */}
-          <TabPanel value={value} index={1}>
+          <TabPanel value={value} index={2}>
             <PMOrderTable orders={orderData} />
           </TabPanel>
 
           {/* History */}
-          <TabPanel value={value} index={2}></TabPanel>
+          <TabPanel value={value} index={3}></TabPanel>
         </Box>
       </div>
     </>
