d4d00e194f4d1f4a46ad36f616e963478a7f0ae9
Kasun M <kasunldrago77@gmail.com>
2022-10-24 10:28:11 +0530

    Add Managing ID to User in user mgmt

--- apps/user-mgmt/src/app/users/schemas/user.schema.ts
+++ apps/user-mgmt/src/app/users/schemas/user.schema.ts
@@ -4,18 +4,21 @@ import { Document } from 'mongoose';
 export type UserDocument = User & Document;
 
 @Schema()
-export class User{
-    @Prop({ required: true })
-    email: string;
+export class User {
+  @Prop({ required: true })
+  email: string;
 
-    @Prop({ required: true })
-    role: string;
+  @Prop({ required: true })
+  role: string;
 
-    @Prop({ required: true })
-    company_name: string;
+  @Prop({ required: true })
+  company_name: string;
 
-    @Prop({ required: true })
-    company_id: string;
+  @Prop({ required: true })
+  company_id: string;
+
+  @Prop()
+  managing_id: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
