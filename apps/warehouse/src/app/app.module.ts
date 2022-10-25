74f339b71cfa9c6d7636068ce7088a75c8eece91
Dumindu <dumindu.chath@gmail.com>
2022-10-26 00:15:38 +0530

    Add CRUD for warehouse items

--- apps/warehouse/src/app/app.module.ts
+++ apps/warehouse/src/app/app.module.ts
@@ -4,12 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Warehouse, WarehouseSchema } from './schemas/warehouse.schema';
+import { StoreItem, StoreItemSchema } from './schemas/storeitem.schema';
 import { WarehouseService } from './services/warehouse.service';
 import { WarehouseController } from './controllers/warehouse.controller';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb+srv://admin:nESDm2LrDARJWLfs@cluster0.m7waswd.mongodb.net/test?retryWrites=true&w=majority'),
-    MongooseModule.forFeature([{name: Warehouse.name, schema: WarehouseSchema}])  
+    MongooseModule.forFeature([{name: Warehouse.name, schema: WarehouseSchema}]), MongooseModule.forFeature([{name: StoreItem.name, schema: StoreItemSchema}])  
   ],
   controllers: [AppController, WarehouseController],
   providers: [AppService, WarehouseService],
--- apps/warehouse/src/app/services/warehouse.service.ts
+++ apps/warehouse/src/app/services/warehouse.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Warehouse, WarehouseDocument } from "../schemas/warehouse.schema";
+import { StoreItem, StoreItemDocument } from "../schemas/storeitem.schema";
 
 @Injectable()
 export class WarehouseService {
 
-    constructor(@InjectModel(Warehouse.name) private warehouseModel: Model<WarehouseDocument>) {}
+    constructor(@InjectModel(Warehouse.name) private warehouseModel: Model<WarehouseDocument>, @InjectModel(StoreItem.name) private storeitemModel: Model<StoreItemDocument>) {}
     
     async create(warehouse: Warehouse): Promise<Warehouse> {
         const newWarehouse = new this.warehouseModel(warehouse);
@@ -43,6 +44,23 @@ export class WarehouseService {
 
     async delete(id): Promise<any> {
         return await this.warehouseModel.findByIdAndRemove(id);
-}
+    }
 
+    //items
+    async readAllItems(id): Promise<StoreItem[]> {
+        return await this.storeitemModel.find({warehouse_id:id}).exec();
+    }
+
+    async createItem(storeitem: StoreItem): Promise<StoreItem> {
+        const newStoreItem = new this.storeitemModel(storeitem);
+        return newStoreItem.save();
+    }
+
+    async updateItem(id, storeitem: StoreItem): Promise<StoreItem> {
+        return await this.storeitemModel.findByIdAndUpdate(id, storeitem, {new: true})
+    }
+
+    async deleteItem(id): Promise<any> {
+        return await this.storeitemModel.findByIdAndRemove(id);
+    }
 }
